53b69448b6b2b57a5c98407e250f478fe10e782a
marekor555 <marekor555@interia.pl>
2026-01-06 12:24:06 +0100

    grafika wektorowa do paradoksu galileusza + wysrodkowanie tekstu w SECTION_TEXT

--- src/components/paradoksy/paradoks_galileo.tsx
+++ src/components/paradoksy/paradoks_galileo.tsx
@@ -1,4 +1,6 @@
 import {TITLE, SECTION_TITLE, SECTION_TEXT, CARD} from "@/components/ui/format_tags.tsx";
+import GalileoVector from "@/assets/galileo.svg";
+
 
 export default function Paradoks_galileo() {
     return (
@@ -11,7 +13,6 @@ export default function Paradoks_galileo() {
             </SECTION_TEXT>
 
             <SECTION_TITLE>Na czym polega paradoks:</SECTION_TITLE>
-
             <SECTION_TEXT>
                 Galileusz zauważył bardzo niezwykłą własność liczb naturalnych i liczb będących ich kwadratami (1, 4, 9, 16…).<br />
                 Z jednej strony:<br />
@@ -23,15 +24,15 @@ export default function Paradoks_galileo() {
 
                 Galileusz wskazał, że możemy stworzyć doskonałą parę:<br />
                 każdej liczbie naturalnej odpowiada dokładnie jeden jej kwadrat:<br />
-                1 ↔ 1, 2 ↔ 4, 3 ↔ 9, 4 ↔ 16, …<br />
+                <img className="w-[50%]" style={{ display: "block", margin: "0 auto" }} src={GalileoVector} alt="Odwzorowanie 1:1 liczb i ich kwadratów" />
 
                 Jeśli między dwoma zbiorami da się stworzyć taką jednoznaczną parę (odpowiedniość 1:1), to w pewnym sensie mają tyle samo elementów.<br /><br />
 
                 To prowadzi do pozornie sprzecznego wniosku:<br />
                 O ile popatrzymy na liczby skończone, kwadratów jest mniej;<br />
                 ale w nieskończoności — jest ich tyle samo co liczb naturalnych.<br />
-
             </SECTION_TEXT>
+
             <SECTION_TITLE>Co to ujawniło o nieskończoności: </SECTION_TITLE>
             <SECTION_TEXT>Galileusz jako jeden z pierwszych zauważył, że nieskończone zbiory nie zachowują się tak jak zbiory skończone. Paradoks ten zasygnalizował, że intuicje z liczb skończonych zawodzą i że do badania nieskończoności potrzebne są nowe pojęcia i narzędzia matematyczne. Dopiero 250 lat później Georg Cantor stworzył pełną teorię mocy zbiorów, która wyjaśniła takie zjawiska. </SECTION_TEXT>
         </CARD>
